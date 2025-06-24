dc9746d8774a35cc93b1529af2ee0fb32871e04d
Truk <hanqhoa01@gmail.com>
2025-06-25 01:15:58 +0700

    Update get all feedbacks api

--- API/services/feedback_service.py
+++ API/services/feedback_service.py
@@ -1,16 +1,32 @@
+from flask import request
+
 from API.firebase_config import db
 from API.utils.generate_id import generate_custom_id
 from datetime import datetime
 
 collection_name = "feedback"
 def get_all_feedback():
-    feedbacks = db.collection(collection_name).stream()
-    return [fb.to_dict() | {"id": fb.id} for fb in feedbacks]
+    limit = int(request.args.get('limit', 100))
+    start_after_id = request.args.get('start_after_id')
+
+    col_ref = db.collection(collection_name).order_by('__name__')
+
+    if start_after_id:
+        start_after_doc = db.collection(collection_name).document(start_after_id).get()
+        if start_after_doc.exists:
+            col_ref = col_ref.start_after(start_after_doc)
+
+    docs = col_ref.limit(limit).stream()
+    feedbacks = [doc.to_dict() | {"id": doc.id} for doc in docs]
+    return feedbacks
 
 def get_feedback_by_id(feedback_id):
-    doc = db.collection(collection_name).document(feedback_id).get()
+    doc_ref = db.collection(collection_name).document(feedback_id)
+    doc = doc_ref.get()
     if doc.exists:
-        return doc.to_dict()
+        data = doc.to_dict()
+        data['id'] = doc.id
+        return data
     return None
 
 
@@ -20,7 +36,7 @@ def create_feedback(data):
         "clauses_sentiment", "status", "response"
     ]
 
-    #data["create_at"] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    data["create_at"] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
     missing_fields = [field for field in required_fields if field not in data]
     if missing_fields:
